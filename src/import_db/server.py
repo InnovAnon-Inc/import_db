3050343a6895ca1a56a7334f64ac59ed65f12f51
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2023-10-06 11:20:24 -0500

    update

--- src/import_db/server.py
+++ src/import_db/server.py
@@ -44,10 +44,10 @@ def create_app(console):
   return app
 
 def start_server(password, username, upstreams=False, upstreamh='0.0.0.0', upstreamp=55553, host="0.0.0.0", port=65432, *args, **kwargs):
-  client = MsfRpcClient(password, username=username, ssl=upstreams, server=upstreamh, port=upstreamp)
-  c_id = client.call(MsfRpcMethod.ConsoleCreate)['id']
+  client  = MsfRpcClient(password, username=username, ssl=upstreams, server=upstreamh, port=upstreamp)
+  c_id    = client.call(MsfRpcMethod.ConsoleCreate)['id']
   console = client.consoles.console(c_id)
-  app = create_app(console, **kwargs)
+  app     = create_app(console, **kwargs)
   app.run(debug=True, host=host, port=port, *args)
 
 # https://www.easydevguide.com/posts/curl_upload_flask
